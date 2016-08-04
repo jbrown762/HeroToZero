d6516fd52d45f9088f32d356038043978b0c06f1
Joe Brown <jbrown762@gmail.com>
2016-08-04 09:35:31 -0400

    CODE

--- src/com/herotozero/Board.java
+++ src/com/herotozero/Board.java
@@ -2,5 +2,31 @@ package com.herotozero;
 
 public class Board
 {
+	int rows;
+	int columns;
+	boolean gameOver = false;
+	
+	public Board(int rows, int columns)
+	{
+		this.rows = rows;
+		this.columns = columns;
+	}
+	
+	public static void main(String[] args)
+	{
+		Board b = new Board(10, 10);
+		
+		b.gameLoop();
+		
+	}
 
+	private void gameLoop()
+	{
+		while(!gameOver){
+			
+			
+			
+		}
+		
+	}
 }
